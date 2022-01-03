a545e97372fc37af09916a0eb8a39991a7a18529
DankoT20 <82830934+DankoT20@users.noreply.github.com>
2022-01-03 16:56:25 +0100

    Kő papír olló

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+lehetoseg = ["kő", "papír", "olló"]
+def jatek():
+    db = 3
+    jatekosNyert = 0
+    gepNyert = 0
+    while True:
+        if db > 0:
+            print(f"Jelenlegi állás: \nJátékos:{jatekosNyert}\nGép:{gepNyert}\n ")
+            beValasztas = input("Add meg, hogy mit szeretnél választani: ")
+            randomValasztas = lehetoseg[random.randint(0, len(lehetoseg)-1)]
+            print(f"A gép választása {randomValasztas}")
+            if beValasztas == randomValasztas:
+                print("Döntetlen!\n")
+            elif beValasztas == "kő" and randomValasztas == "olló":
+                jatekosNyert += 1
+                db -= 1
+            elif beValasztas == "kő" and randomValasztas == "papír":
+                gepNyert += 1
+                db -=1
+            elif beValasztas == "papír" and randomValasztas == "kő":
+                jatekosNyert += 1
+                db -= 1
+            elif beValasztas == "papír" and randomValasztas == "olló":
+                gepNyert += 1
+                db -= 1
+            elif beValasztas == "olló" and randomValasztas == "papír":
+                jatekosNyert += 1
+                db -= 1
+            elif beValasztas == "olló" and randomValasztas == "kő":
+                gepNyert += 1
+                db -= 1
+        else:
+            if jatekosNyert > gepNyert:
+                print("A játékos nyert!\n")
+                break
+            else:
+                print("Sajnos a játékos vesztett!\n")
+                break
+
+jatek()
